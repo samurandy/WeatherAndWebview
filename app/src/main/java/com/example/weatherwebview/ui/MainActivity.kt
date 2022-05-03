cac31340b4f9784ec970eab5409e4a419d48d409
samurandy <andreschv@gmail.com>
2022-05-03 20:03:53 +0200

    Added clean architecture folders

--- app/src/main/java/com/example/weatherwebview/ui/MainActivity.kt
+++ app/src/main/java/com/example/weatherwebview/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.weatherwebview
+package com.example.weatherwebview.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.weatherwebview.R
 import com.example.weatherwebview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
