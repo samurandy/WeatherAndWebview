c246fc41a1865cbf737a494c665f06677d196e4f
samurandy <andreschv@gmail.com>
2022-05-10 18:49:51 +0200

    Change .gitignore

--- app/src/main/java/com/example/weatherwebview/data/network/RemoteService.kt
+++ app/src/main/java/com/example/weatherwebview/data/network/RemoteService.kt
@@ -1,6 +1,7 @@
 package com.example.weatherwebview.data.network
 
 import com.example.weatherwebview.data.network.model.RemoteResult
+import com.example.weatherwebview.utils.Credentials
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,7 +10,6 @@ interface RemoteService {
     companion object{
         const val latitude: String = "51.274424298528906"
         const val longitude: String = "6.862137434544318"
-        const val apiKey: String = "5c3f77686f3e0c2c477e729384327ebb"
         const val exclude: String = "minutely,hourly"
         const val units: String = "metric"
     }
@@ -20,7 +20,7 @@ interface RemoteService {
         @Query("lon") longitude: String = RemoteService.longitude,
         @Query("exclude") exclude: String = RemoteService.exclude,
         @Query("units") units: String = RemoteService.units,
-        @Query("appid") apiKey: String = RemoteService.apiKey
+        @Query("appid") apiKey: String = Credentials.apiKey
 
     ): Response<RemoteResult>
 }
